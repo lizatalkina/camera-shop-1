99e8a24be9bdcb1c4aca4acc68624f33377cad13
lizatalkina <liza.talkina@gmail.com>
2023-01-31 15:57:25 +0300

    Добавляет страницы проекта

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,34 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { AppRoute } from '../../const';
+import CatalogScreen from '../../pages/catalog-screen/catalog-screen';
+import ProductScreen from '../../pages/product-screen/product-screen';
+import BasketScreen from '../../pages/basket-screen/basket-screen';
+
 function App(): JSX.Element {
-  return <p>Hello, world!</p>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path = { AppRoute.Catalog }
+          element = {
+            <CatalogScreen/>
+          }
+        />
+        <Route
+          path= { AppRoute.Product }
+          element = {
+            <ProductScreen/>
+          }
+        />
+        <Route
+          path = { AppRoute.Basket }
+          element = {
+            <BasketScreen/>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ project/src/const.ts
@@ -0,0 +1,5 @@
+export enum AppRoute {
+  Catalog = '/',
+  Product = '/catalog/:id',
+  Basket = '/basket',
+}
