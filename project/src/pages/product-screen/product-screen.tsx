940b6f4989df8fa8ddfc7694d2d8884f912dd759
lizatalkina <liza.talkina@gmail.com>
2023-02-06 21:29:53 +0300

    Добавляет характеристики и описание товара

--- project/src/pages/product-screen/product-screen.tsx
+++ project/src/pages/product-screen/product-screen.tsx
@@ -8,11 +8,20 @@ import { useAppDispatch, useAppSelector } from '../../hooks';
 import { fetchCameraAction } from '../../store/api-actions';
 import { useEffect } from 'react';
 import CatalogScreen from '../catalog-screen/catalog-screen';
+import ProductTabs from '../../components/product-tabs/product-tabs';
+import { useState } from 'react';
 
 function ProductScreen (): JSX.Element {
   const { id } = useParams();
   const dispatch = useAppDispatch();
   const camera = useAppSelector((state) => state.camera);
+  const [specifications, setSpecifications] = useState(false);
+  const [information, setInformation] = useState(true);
+
+  const handleClick = () => {
+    setInformation(!information);
+    setSpecifications(!specifications);
+  };
 
   useEffect(() => {
     dispatch(fetchCameraAction(String(id)));
@@ -60,33 +69,16 @@ function ProductScreen (): JSX.Element {
                   </button>
                   <div className="tabs product__tabs">
                     <div className="tabs__controls product__tabs-controls">
-                      <button className="tabs__control" type="button">Характеристики</button>
-                      <button className="tabs__control is-active" type="button">Описание</button>
+                      <button className={specifications ? 'tabs__control is-active' : 'tabs__control'} type="button"
+                        onClick = {() => handleClick()}
+                      >Характеристики
+                      </button>
+                      <button className={information ? 'tabs__control is-active' : 'tabs__control'} type="button"
+                        onClick = {() => handleClick()}
+                      >Описание
+                      </button>
                     </div>
-                    <div className="tabs__content">
-                      <div className="tabs__element">
-                        <ul className="product__tabs-list">
-                          <li className="item-list"><span className="item-list__title">Артикул:</span>
-                            <p className="item-list__text"> DA4IU67AD5</p>
-                          </li>
-                          <li className="item-list"><span className="item-list__title">Категория:</span>
-                            <p className="item-list__text">Видеокамера</p>
-                          </li>
-                          <li className="item-list"><span className="item-list__title">Тип камеры:</span>
-                            <p className="item-list__text">Коллекционная</p>
-                          </li>
-                          <li className="item-list"><span className="item-list__title">Уровень:</span>
-                            <p className="item-list__text">Любительский</p>
-                          </li>
-                        </ul>
-                      </div>
-                      <div className="tabs__element is-active">
-                        <div className="product__tabs-text">
-                          <p>Немецкий концерн BRW разработал видеокамеру Das Auge IV в&nbsp;начале 80-х годов, однако она до&nbsp;сих пор пользуется популярностью среди коллекционеров и&nbsp;яростных почитателей старинной техники.</p>
-                          <p>Вы&nbsp;тоже можете прикоснуться к&nbsp;волшебству аналоговой съёмки, заказав этот чудо-аппарат. Кто знает, может с&nbsp;Das Auge IV&nbsp;начнётся ваш путь к&nbsp;наградам всех престижных кинофестивалей.</p>
-                        </div>
-                      </div>
-                    </div>
+                    <ProductTabs camera = {camera} specifications = {specifications} information = {information}/>
                   </div>
                 </div>
               </div>
